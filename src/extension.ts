173d9e13fa5d5d9f93a3d8c642e3a68253f49b32
Rongxin Liu <rongxinliu.dev@gmail.com>
2024-10-09 09:11:34 +0100

    add help50.ask and help50.say

--- src/extension.ts
+++ src/extension.ts
@@ -24,8 +24,36 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("help50.run", async (args) => {
-      console.log("help50.run");
+    vscode.commands.registerCommand("help50.ask", async (args) => {
+      try {
+        const errorMessage = args[0];
+        const ddb50 = vscode.extensions.getExtension("cs50.ddb50");
+        const api = ddb50!.exports;
+        const displayMessage = "Explain terminal error";
+        const payload = {
+          api: "/api/v1/terminal",
+          config: "chat_cs50",
+          error_message: errorMessage,
+          stream: true
+        };
+        const contextMessage = `${displayMessage}:\n\`\`\`${args[1]}`;
+        api.requestGptResponse(displayMessage, contextMessage, payload);
+      } catch (error) {
+        console.error(error);
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("help50.say", async (args) => {
+      try {
+        const ddbMessage = args[0];
+        const ddb50 = vscode.extensions.getExtension("cs50.ddb50");
+        const api = ddb50!.exports;
+        api.requestDuckSay(ddbMessage);
+      } catch (error) {
+        console.error(error);
+      }
     })
   );
 }
